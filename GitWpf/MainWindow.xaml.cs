1493910d85f6a07b65f6696efe0888c322e8a5b1
ELCOT <monika17833@gmail.com>
2025-04-05 20:59:13 +0530

    Add project files.

---
+++ GitWpf/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GitWpf;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Login_Click(object sender, RoutedEventArgs e)
+    {
+        string email = txtEmail.Text.Trim();
+        string password = txtPassword.Password;
+
+        if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+        {
+            MessageBox.Show("Please enter Email and Password.", "Validation", MessageBoxButton.OK, MessageBoxImage.Warning);
+            return;
+        }
+
+        // Login success -> Open new window
+        DashboardWindow dashboard = new DashboardWindow();
+        dashboard.Show();
+        this.Close();
+    }
+
+}
